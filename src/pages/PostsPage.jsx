cad66d074531735cfc66680c253a6a93b19f29e5
Gvidas <gvidas.brazionis@gmail.com>
2024-01-05 12:10:46 +0200

    removed console logs

--- src/pages/PostsPage.jsx
+++ src/pages/PostsPage.jsx
@@ -8,7 +8,6 @@ function PostsPage() {
 
   return (
     <div>
-      <button onClick={() => console.log(posts)}>CLG</button>
       <SortPosts />
       <div className='all-posts'>
         {posts.map((post) => (
